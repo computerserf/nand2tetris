ed8415194d8fa71e1cfeb59102b4c36b4f942834
computerserf <52532030+computerserf@users.noreply.github.com>
2020-08-25 19:02:09 +0530

    created initial Assembler project files

---
+++ projects/06/Assembler/Assembler.cpp
@@ -0,0 +1,12 @@
+﻿// Assembler.cpp : Defines the entry point for the application.
+//
+
+#include "Assembler.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
