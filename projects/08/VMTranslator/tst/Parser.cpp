f27dc0bcf435b2156c2e8df3602c9bfb9ac98e2c
Haresh Bhagchandani <hbhagchandani@gmail.com>
2020-10-22 17:11:16 +0530

    added test files

--- projects/08/VMTranslator/tst/Parser.cpp
+++ projects/08/VMTranslator/tst/Parser.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020 Haresh Bhachandani
- *
+ * 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
@@ -9,10 +9,10 @@
  * copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following
  * conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -23,42 +23,11 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
- /* Interface of the CodeWriter module.
-  */
+#include <gtest/gtest.h>
 
-#ifndef CODE_WRITER_H
-#define CODE_WRITER_H
+TEST(ParserTest, Test1) {
 
-#include "Common.h"
+    ASSERT_TRUE(true);
 
-#include <string>
-#include <ostream>
+}
 
-// Refer to the API documentation in chapter 7
-class CodeWriter
-{
-public:
-	CodeWriter(std::ostream &outputStream);
-	void setFilename(std::string filename);
-	void writeArithmetic(std::string command);
-	void writePushPop(CommandType type, std::string segment, int index);
-    void writeAnnotation(CommandType 
-type, std::string argument1, std::string argument2);
-
-private:
-	std::ostream &out;
-	std::string prefix;
-	unsigned int branchCount;
-
-	inline std::string branchLabel()
-	{
-		return prefix + ".vm" + "$" + std::to_string(branchCount);
-	}
-	
-	inline std::string file_prefix()
-    {
-        return prefix;
-    }
-};
-
-#endif // CODE_WRITER_H
